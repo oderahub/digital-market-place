5a3ac45d1d161f3d07186d992e4fb9c319dce7d8
oderahub <okere_chidera@yahoo.com>
2024-09-05 21:28:37 +0100

    [FEAT]: cart

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,17 +1,40 @@
 'use client'
-import { act, useState } from "react"
+import {  useState, useRef, useEffect } from "react"
 import { PRODUCT_CATEGORIES } from "./config"
 import NavItem from "./NavItem"
+import {useOnClickOutside} from "@/hooks/use-on-click-outside"
 
 
 const NavItems = () =>{
     const [activeIndex, setActiveIndex] = useState<null | Number>(null)
 
-   
+   // Escape keyboard fuction to remove the dropdown from the Navbar
+      useEffect(()=>{
+        const handler = (e: KeyboardEvent)=>{
+            if(e.key === 'Escape') {
+                setActiveIndex(null)
+            }
+        }
+        document.addEventListener("keydown", handler)
+
+        return ()=>{
+            document.removeEventListener('keydown', handler)
+        }
+      }, [])
+
+
     const isAnyOpen = activeIndex !==null
 
+    const navRef = useRef<HTMLDivElement | null>(null)
+
+    useOnClickOutside(navRef, () => setActiveIndex(null))
+
+     const close = () => {
+    setActiveIndex(null);
+  };
+
  return (
-    <div className="flex gap-4 h-full">
+    <div className="flex gap-4 h-full" ref={navRef}>
      {PRODUCT_CATEGORIES.map((category, i)=>{
       
       // to track which index that is currently open and one that is not on the Navbar
@@ -24,8 +47,12 @@ const NavItems = () =>{
       }
       const isOpen = i === activeIndex
       return (
-         <NavItem category={category} handleOpen={handleOpen} isOpen={isOpen} 
-         key={category.value} isAnyOpen={isAnyOpen} />
+         <NavItem category={category} 
+         handleOpen={handleOpen} 
+         isOpen={isOpen}
+          key={category.value} 
+          isAnyOpen={isAnyOpen} 
+          close={close} />
       )
      })}
     </div>
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,23 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+export function formatPrice(
+  price: number | string,
+  options: {
+    currency?: "USD" | "EUR" | "GBP" | "BDT" | "NGR"
+    notation?: Intl.NumberFormatOptions["notation"] 
+  } = {}
+) {
+  const {currency = "USD", notation = "compact"} = options
+
+  const numericPrice = typeof price === "string" ? parseFloat(price) : price
+
+  return new Intl.NumberFormat("en-Us", {
+    style: "currency",
+    currency,
+    notation,
+    maximumFractionDigits: 2
+  }).format(numericPrice)
+}
