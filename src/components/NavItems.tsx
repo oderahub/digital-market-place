58bde1817c0b2b8fc87c81d9045a4c0100fa6d63
oderahub <okere_chidera@yahoo.com>
2024-09-13 17:14:13 +0100

    feat:setup product collections & img upload

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,63 +1,61 @@
 'use client'
-import {  useState, useRef, useEffect } from "react"
-import { PRODUCT_CATEGORIES } from "./config"
-import NavItem from "./NavItem"
-import {useOnClickOutside} from "@/hooks/use-on-click-outside"
+import { useState, useRef, useEffect } from 'react'
+import { PRODUCT_CATEGORIES } from '../config'
+import NavItem from './NavItem'
+import { useOnClickOutside } from '@/hooks/use-on-click-outside'
 
+const NavItems = () => {
+  const [activeIndex, setActiveIndex] = useState<null | Number>(null)
 
-const NavItems = () =>{
-    const [activeIndex, setActiveIndex] = useState<null | Number>(null)
-
-   // Escape keyboard fuction to remove the dropdown from the Navbar
-      useEffect(()=>{
-        const handler = (e: KeyboardEvent)=>{
-            if(e.key === 'Escape') {
-                setActiveIndex(null)
-            }
-        }
-        document.addEventListener("keydown", handler)
-
-        return ()=>{
-            document.removeEventListener('keydown', handler)
-        }
-      }, [])
-
-
-    const isAnyOpen = activeIndex !==null
-
-    const navRef = useRef<HTMLDivElement | null>(null)
-
-    useOnClickOutside(navRef, () => setActiveIndex(null))
-
-     const close = () => {
-    setActiveIndex(null);
-  };
-
- return (
-    <div className="flex gap-4 h-full" ref={navRef}>
-     {PRODUCT_CATEGORIES.map((category, i)=>{
-      
-      // to track which index that is currently open and one that is not on the Navbar
-      const handleOpen = () =>{
-        if (activeIndex === i) {
-            setActiveIndex(null)
-        }else {
-            setActiveIndex(i)
-        }
+  // Escape keyboard fuction to remove the dropdown from the Navbar
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      if (e.key === 'Escape') {
+        setActiveIndex(null)
       }
-      const isOpen = i === activeIndex
-      return (
-         <NavItem category={category} 
-         handleOpen={handleOpen} 
-         isOpen={isOpen}
-          key={category.value} 
-          isAnyOpen={isAnyOpen} 
-          close={close} />
-      )
-     })}
-    </div>
- )
+    }
+    document.addEventListener('keydown', handler)
 
+    return () => {
+      document.removeEventListener('keydown', handler)
+    }
+  }, [])
+
+  const isAnyOpen = activeIndex !== null
+
+  const navRef = useRef<HTMLDivElement | null>(null)
+
+  useOnClickOutside(navRef, () => setActiveIndex(null))
+
+  const close = () => {
+    setActiveIndex(null)
+  }
+
+  return (
+    <div className="flex gap-4 h-full" ref={navRef}>
+      {PRODUCT_CATEGORIES.map((category, i) => {
+        // to track which index that is currently open and one that is not on the Navbar
+        const handleOpen = () => {
+          if (activeIndex === i) {
+            setActiveIndex(null)
+          } else {
+            setActiveIndex(i)
+          }
+        }
+        const isOpen = i === activeIndex
+        return (
+          <NavItem
+            category={category}
+            handleOpen={handleOpen}
+            isOpen={isOpen}
+            key={category.value}
+            isAnyOpen={isAnyOpen}
+            close={close}
+          />
+        )
+      })}
+    </div>
+  )
 }
 
 export default NavItems
