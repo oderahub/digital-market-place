ac5de36cae9a97e71a64b6a771193513d4b133eb
oderahub <okere_chidera@yahoo.com>
2024-09-27 11:37:22 +0100

    feat:checkout page

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -69,7 +69,7 @@ const Cart = () => {
               <SheetFooter>
                 <SheetTrigger asChild>
                   <Link
-                    href="./cart"
+                    href="/cart"
                     className={buttonVariants({
                       className: 'w-full '
                     })}
