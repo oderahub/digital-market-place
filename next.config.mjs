d1e194b6090e74378a46b9bf6c2226be6f670681
oderahub <okere_chidera@yahoo.com>
2024-10-05 19:39:36 +0100

    build:fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,29 +1,7 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         hostname: 'digital-market-place-nine.vercel.app',
-//         pathname: '**',
-//         port: '3000',
-//         protocol: 'https'
-//       }
-//     ]
-//   }
-// }
-
-// export default nextConfig
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        hostname: 'digital-market-place-nine.vercel.app',
-        pathname: '**',
-        protocol: 'https'
-      }
-    ],
-    domains: ['digital-market-place-nine.vercel.app']
+    domains: ['localhost', 'digital-market-place-nine.vercel.app']
   }
 }
 
