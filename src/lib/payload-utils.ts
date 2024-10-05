80399abd5882fff6317ce25045161faafc8c5312
oderahub <okere_chidera@yahoo.com>
2024-10-05 19:51:07 +0100

    fix: user token

--- src/lib/payload-utils.ts
+++ src/lib/payload-utils.ts
@@ -1,21 +1,25 @@
-import { ReadonlyRequestCookies } from "next/dist/server/web/spec-extension/adapters/request-cookies";
-import { NextRequest } from "next/server";
-import { User } from "../payload-types";
-
+import { ReadonlyRequestCookies } from 'next/dist/server/web/spec-extension/adapters/request-cookies'
+import { NextRequest } from 'next/server'
+import { User } from '../payload-types'
 
 export const getServerSideUser = async (
-    cookies: NextRequest["cookies"] | ReadonlyRequestCookies
+  cookies: NextRequest['cookies'] | ReadonlyRequestCookies
 ) => {
-    const token = cookies.get("payload-token")?.value
+  const token = cookies.get('payload-token')?.value
 
-    const meRes = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/users/me`, {
-        headers: {
-            Authorization: `JWT ${token}`
-        },
+  // If no token is found, return null (user is not signed in)
+  if (!token) {
+    return { user: null }
+  }
+
+  // Fetch user data if token exists
+  const meRes = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/users/me`, {
+    headers: {
+      Authorization: `JWT ${token}`
     }
-)
-const {user} = (await meRes.json()) as {
-    user : User | null
-}
-return { user }
+  })
+  const { user } = (await meRes.json()) as {
+    user: User | null
+  }
+  return { user }
 }
