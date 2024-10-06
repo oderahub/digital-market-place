9f026f499b582f03c9bd1b1b285160e382ee35a4
oderahub <okere_chidera@yahoo.com>
2024-10-06 08:52:27 +0100

    fix:case sensitive img path

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,45 +1,44 @@
 export const PRODUCT_CATEGORIES = [
-{
+  {
     label: 'UI Kits',
     value: 'ui_kits' as const,
     featured: [
-        {
-            name: 'Editor picks',
-            href: '#',
-            imageSrc: '/nav/ui-kits/mixed.jpg'
-        },
-        {
-            name: 'New Arrivals',
-            href: '#',
-            imageSrc: '/nav/ui-kits/blue.jpg'
-        },
-        {
-            name: 'Bestseller',
-            href: '#',
-            imageSrc: '/nav/ui-kits/purple.jpg'
-        }
+      {
+        name: 'Editor picks',
+        href: '#',
+        imageSrc: '/Nav/ui-kits/mixed.jpg'
+      },
+      {
+        name: 'New Arrivals',
+        href: '#',
+        imageSrc: '/Nav/ui-kits/blue.jpg'
+      },
+      {
+        name: 'Bestseller',
+        href: '#',
+        imageSrc: '/Nav/ui-kits/purple.jpg'
+      }
     ]
-},
-{
+  },
+  {
     label: 'Icons',
     value: 'icons' as const,
     featured: [
-        {
-            name: 'Favorite Icon Picks',
-            href: '#',
-            imageSrc: '/nav/Icons/picks.jpg'
-        },
-        {
-            name: 'New Arrivals',
-            href: '#',
-            imageSrc: '/nav/Icons/new.jpg'
-        },
-        {
-            name: 'Bestsellering Icons',
-            href: '#',
-            imageSrc: '/nav/Icons/bestsellers.jpg'
-        }
+      {
+        name: 'Favorite Icon Picks',
+        href: '#',
+        imageSrc: '/Nav/Icons/picks.jpg'
+      },
+      {
+        name: 'New Arrivals',
+        href: '#',
+        imageSrc: '/Nav/Icons/new.jpg'
+      },
+      {
+        name: 'Bestsellering Icons',
+        href: '#',
+        imageSrc: '/Nav/Icons/bestsellers.jpg'
+      }
     ]
-}
-
+  }
 ]
