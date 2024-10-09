b7d484d43b1a3f279f6b77a22b161bdfc53c2c11
oderahub <okere_chidera@yahoo.com>
2024-10-09 07:37:14 +0100

    fix: clear cart

--- src/components/PaymentStatus.tsx
+++ src/components/PaymentStatus.tsx
@@ -2,8 +2,8 @@
 
 import { trpc } from '@/trpc/client'
 import { useRouter } from 'next/navigation'
-import { useEffect } from 'react'
-import { date } from 'zod'
+import { useEffect, useCallback } from 'react'
+import { useCart } from '../hooks/use-cart'
 
 interface PaymentStatusProps {
   orderEmail: string
@@ -12,20 +12,29 @@ interface PaymentStatusProps {
 }
 
 const PaymentStatus = ({ orderEmail, orderId, isPaid }: PaymentStatusProps) => {
-  // calling the polling from payment router
   const router = useRouter()
+  const { clearCart } = useCart()
 
   const { data } = trpc.payment.pollingOrderStatus.useQuery(
     { orderId },
     {
-      enabled: isPaid === false,
-      refetchInterval: (data) => (data?.isPaid ? false : 1000)
+      enabled: true,
+      refetchInterval: (data) => (data?.isPaid ? false : 1000),
     }
   )
 
+  const handlePaymentComplete = useCallback(() => {
+    clearCart()
+    router.refresh()
+  }, [clearCart, router])
+
   useEffect(() => {
-    if (data?.isPaid) router.refresh()
-  }, [data?.isPaid, router])
+    if (data?.isPaid) {
+      handlePaymentComplete()
+    } else {
+      clearCart()
+    }
+  }, [data?.isPaid, handlePaymentComplete, clearCart])
 
   return (
     <div className="mt-16 grid grid-cols-2 gap-x-4 text-sm text-gray-600">
@@ -36,7 +45,7 @@ const PaymentStatus = ({ orderEmail, orderId, isPaid }: PaymentStatusProps) => {
 
       <div>
         <p className="font-medium text-gray-900">Order status</p>
-        <p>{isPaid ? 'Payment sucessful' : 'Pending payment'}</p>
+        <p>{isPaid ? 'Payment successful' : 'Pending payment'}</p>
       </div>
     </div>
   )
